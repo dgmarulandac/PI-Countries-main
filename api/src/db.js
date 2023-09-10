7d1f9ae2668b9cea65920e9952fffa2f777e1e29
dgmarulandac <54507534+dgmarulandac@users.noreply.github.com>
2023-09-09 19:37:30 -0500

    correct db conexion

--- api/src/db.js
+++ api/src/db.js
@@ -15,11 +15,7 @@ const {
   const sequelize = new Sequelize(`${DB_DEPLOY}`,   {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions: {
-    ssl: {
-      require: true
-    }
-  }
+
 });
 
 
